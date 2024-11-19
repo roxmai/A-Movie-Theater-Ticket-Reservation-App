238f66828d8ad6ad42397bd21f7024f5bb3cca96
Jason123Xu <zhuoyan.xu@ucalgary.ca>
2024-11-19 10:53:42 -0700

    payment

--- Backend/src/main/java/com/example/AcmePlex/paymentsystem/PaymentSystem.java
+++ Backend/src/main/java/com/example/AcmePlex/paymentsystem/PaymentSystem.java
@@ -1,26 +1,42 @@
 package com.example.acmeplex.paymentsystem;
 
+import org.joda.time.DateTime;
+
 public class PaymentSystem {
+    private int TicketId;
     private int paymentId;
     private double amount;
     private String paymentMethod;
     private boolean paymentStatus;
+    private String lastUpdateTime;
 
-    public PaymentSystem(int paymentId, double amount, String paymentMethod) {
-        this.paymentId = paymentId;
+    public PaymentSystem(int TicketId, double amount, String paymentMethod) {
+        this.lastUpdateTime = getCurrentDateTime();
+        this.paymentId=TicketId*10+1;
+        this.TicketId = TicketId;
         this.amount = amount;
         this.paymentMethod = paymentMethod;
     }
 
     public Boolean processPayment() {
         // Process payment logic
+        this.lastUpdateTime = getCurrentDateTime();
         paymentStatus = true;
+
         return paymentStatus;
     }
 
     public Boolean refundPayment() {
         // Refund payment logic
+        this.lastUpdateTime = getCurrentDateTime();
         paymentStatus = false;
         return true;
+    }
+
+    private String getCurrentDateTime(){
+        DateTime dt = new DateTime();
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        String dtStr = fmt.print(dt);
+        return dtStr;
     }
 }
