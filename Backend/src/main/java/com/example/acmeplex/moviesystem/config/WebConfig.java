f24b3e4a9da26204ce15ab03c837547c948f65b4
wjycn2000 <wjy.cn2000@outlook.com>
2024-11-21 15:11:32 -0700

    changes to make app run

--- Backend/src/main/java/com/example/acmeplex/moviesystem/config/WebConfig.java
+++ Backend/src/main/java/com/example/acmeplex/moviesystem/config/WebConfig.java
@@ -6,21 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Component
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:3000") // Allow only this origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow specific HTTP methods
-                .allowedHeaders("*"); // Allow all headers
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/static/images/");
     }
+
 }
